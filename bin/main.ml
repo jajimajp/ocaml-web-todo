58dd81c77f8ef121245dc3ba05e401aa0fdecc18
Yajima <119467077+jajimajp@users.noreply.github.com>
2024-07-09 23:57:10 +0900

    [WIP] single query execution

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,33 @@
-let () = print_endline "Hello, World!"
+let with_conn f ~stdenv =
+  let uri = Uri.of_string "sqlite3://" in
+  Caqti_eio_unix.with_connection uri ~stdenv f
+
+(* module `Q` contains our query definitions *)
+module Q = struct
+  open Caqti_request.Infix
+
+  let add =
+    Caqti_type.(t2 int int ->! int)
+    "SELECT ? + ?"
+end
+
+let add a b (conn : Caqti_eio.connection) =
+  let module Conn = (val conn : Caqti_eio.CONNECTION) in
+  Conn.find Q.add (a, b)
+
+open Printf
+
+type data = int
+
+let main (env : Eio_unix.Stdenv.base) =
+  let ( let* ) = Result.bind in
+  let program : (data, 'err) result =
+    with_conn ~stdenv:(env :> Caqti_eio.stdenv) @@ fun conn ->
+      let* sum = add 1 2 conn in
+      Ok sum in
+
+  match program with
+  | Error err -> failwith (sprintf "Error: %s" (Caqti_error.show err))
+  | Ok sum -> printf "Sum: %d\n" sum
+
+let () = Eio_main.run main
